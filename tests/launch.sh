6987c795aca00e16a6ad884524bf6efdc3c49429
William Desportes <williamdes@wdes.fr>
2021-03-22 15:13:35 +0100

    Fix testing matrix

--- tests/launch.sh
+++ tests/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 command -v pdfinfo > /dev/null
 if [ $? -gt 0 ]; then
@@ -18,11 +18,14 @@ EXAMPLE_FILES="$(find examples/ -type f -name 'example*.php' \
                 | sort -df)"
 TEMP_FOLDER="$(mktemp -d /tmp/WarnockPDF-tests.XXXXXXXXX)"
 OUTPUT_FILE="${TEMP_FOLDER}/output.pdf"
+OUTPUT_FILE_ERROR="${TEMP_FOLDER}/errors.txt"
 ROOT_DIR="$(php -r 'echo realpath(dirname(__FILE__));')"
 TESTS_DIR="${ROOT_DIR}/tests/"
 
 echo "Root folder: ${ROOT_DIR}"
 echo "Temporary folder: ${TEMP_FOLDER}"
+
+FAILED_FLAG=0
 
 for file in $EXAMPLE_FILES; do
     echo "File: $file"
@@ -30,29 +33,39 @@ for file in $EXAMPLE_FILES; do
     if [ $? -eq 0 ]; then
         echo "File-lint-passed: $file"
     fi
-    php -d xdebug.coverage_enable=0 \
-        -d xdebug.default_enable=0 \
+    php -d display_errors=on \
+        -d error_reporting=-1 \
         -d pcov.directory="${ROOT_DIR}" \
         -d auto_prepend_file="${TESTS_DIR}/coverage.php" \
-        "$file" > "${OUTPUT_FILE}"
+        "$file" 1> "${OUTPUT_FILE}" 2> "${OUTPUT_FILE_ERROR}"
     if [ $? -eq 0 ]; then
         echo "File-run-passed: $file"
+        ERROR_LOGS="$(cat "${OUTPUT_FILE_ERROR}")"
+        if [ ! -z "${ERROR_LOGS}" ]; then
+            FAILED_FLAG=1
+            echo "Logs: $file"
+            echo "---------------------------"
+            echo "${ERROR_LOGS}"
+            echo "---------------------------"
+        fi
         if [ $(head -c 4 "${OUTPUT_FILE}") != "%PDF" ]; then
+            FAILED_FLAG=1
             echo "Generated-not-a-pdf: $file"
             echo "---------------------------"
-            cat ${OUTPUT_FILE}
+            cat "${OUTPUT_FILE}"
             echo "---------------------------"
-            exit 1
+            continue
         fi
         pdfinfo "${OUTPUT_FILE}" > /dev/null
         if [ $? -gt 0 ]; then
+            FAILED_FLAG=1
             echo "Generated-invalid-file: $file"
-            exit 1
         fi
     else
         echo "File-run-failed: $file"
-        exit 1
     fi
 done
 
 rm -rf "${TEMP_FOLDER}"
+
+exit "${FAILED_FLAG}"
