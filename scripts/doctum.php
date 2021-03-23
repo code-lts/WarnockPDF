ec2da73ea2da42b446917cbbcee3a51e8bb024dd
William Desportes <williamdes@wdes.fr>
2021-03-23 22:47:26 +0100

    Add manual docs publish and add a footer link for Doctum

--- scripts/doctum.php
+++ scripts/doctum.php
@@ -24,4 +24,12 @@ return new Doctum($iterator, [
     'cache_dir'            => $rootDir . '/cache',
     'source_dir'           => $rootDir,
     'remote_repository'    => new GitHubRemoteRepository('code-lts/warnockpdf', $rootDir),
+    'footer_link'          => [
+        'href'        => 'https://github.com/code-lts/WarnockPDF#readme',
+        'rel'         => 'noreferrer noopener',
+        'target'      => '_blank',
+        'before_text' => 'This documentation is for',
+        'link_text'   => 'WarnockPDF',
+        'after_text'  => 'the fork of TCPDF.',
+    ],
 ]);
