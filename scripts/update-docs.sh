1bc0988b4067b66231a4aaf42ad49621f821b50d
William Desportes <williamdes@wdes.fr>
2021-03-23 22:24:48 +0100

    Remove cache from gh-pages uploads

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Remove cache dir, do not upload it
+rm -rf ./cache
+
 TEMP_DIR="$(mktemp -d --suffix=_doctum-build-warnockpdf)"
 cp -rp build "${TEMP_DIR}"
 git checkout gh-pages
@@ -8,7 +11,11 @@ git ls-files ./api-docs/ | xargs -r -n 1 rm
 rm -rfd ./api-docs/*
 mv "${TEMP_DIR}"/build/* ./
 rm -rf "${TEMP_DIR}"
+
+# Force update the CNAME just in case
 echo 'warnockpdf.long-term.support' > CNAME
+
+# Push the changes, even if they are empty
 git add -A
 git commit --allow-empty -m "Api documentation update ($(date --utc))" -m "#apidocs"
 git push
