1dee3d4f5919d317ba1b478d3ab6a04cfceafbc5
William Desportes <williamdes@wdes.fr>
2021-03-23 22:26:32 +0100

    Publish API docs to api-docs folder

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -2,14 +2,15 @@
 set -e
 
 # Remove cache dir, do not upload it
-rm -rf ./cache
+# Use sudo because previous action was not ran as the same user
+sudo rm -rf ./cache
 
 TEMP_DIR="$(mktemp -d --suffix=_doctum-build-warnockpdf)"
 cp -rp build "${TEMP_DIR}"
 git checkout gh-pages
 git ls-files ./api-docs/ | xargs -r -n 1 rm
 rm -rfd ./api-docs/*
-mv "${TEMP_DIR}"/build/* ./
+mv "${TEMP_DIR}"/build ./api-docs
 rm -rf "${TEMP_DIR}"
 
 # Force update the CNAME just in case
