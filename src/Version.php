2bb1f7e899aedaac43b1b39263c1d57c82bf605f
William Desportes <williamdes@wdes.fr>
2021-03-27 00:07:35 +0100

    Disable phpcs on the version constants

--- src/Version.php
+++ src/Version.php
@@ -11,8 +11,10 @@ namespace WarnockPDF;
  */
 final class Version
 {
+    // phpcs:disable
     const VERSION = '7.0.0';
     const MAJOR = 7;
     const MINOR = 0;
     const PATCH = 1;
+    // phpcs:enable
 }
